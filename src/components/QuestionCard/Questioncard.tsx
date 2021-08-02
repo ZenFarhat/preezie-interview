bf373919c596a6fceb4d47b747f70d78f5a98028
Zen F <zeinfarhat6@gmail.com>
2021-08-03 09:16:43 +1000

    disable on submit added

--- src/components/QuestionCard/Questioncard.tsx
+++ src/components/QuestionCard/Questioncard.tsx
@@ -12,20 +12,18 @@ function Questioncard(props: {
   index: number
 }) {
   const [answer, setAnswer] = useState('')
-  const [score, setScore] = useState(0)
-  const [questionsAnswered, setQuestionsAnswered] = useState(0)
+  const [isDisabled, setDisabled] = useState(false)
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
     if (answer.toLowerCase() === props.correctAnswer.toLowerCase()) {
-      setScore(score + 1)
-      setQuestionsAnswered(questionsAnswered + 1)
-      console.log('Correct answer detected: ' + score)
+      console.log('Correct answer detected')
       setAnswer('')
     } else {
-      console.log('Incorrect answer detected: ' + score)
+      console.log('Incorrect answer detected')
       setAnswer('')
     }
+    setDisabled(true)
   }
 
   console.log(props)
@@ -40,38 +38,40 @@ function Questioncard(props: {
       id={props.id}
     >
       <h1 className='form__header'>{`${props.index}. ${props.topic}`}</h1>
-      {props.type === 'SingleSelect' ? (
-        props.answerOptions?.map((option) => {
-          return (
-            <label className='form__label'>
-              {option}
-              <input
-                type='radio'
-                name='action'
-                id=''
-                required
-                value={option}
-                onChange={(e) => {
-                  setAnswer(e.target.value)
-                }}
-              />
-            </label>
-          )
-        })
-      ) : (
-        <input
-          className='form__text-input'
-          type='text'
-          required
-          value={answer}
-          onChange={(e) => {
-            setAnswer(e.target.value)
-          }}
-        />
-      )}
-      <button className='form__button' type='submit'>
-        Submit Answer
-      </button>
+      <fieldset disabled={isDisabled}>
+        {props.type === 'SingleSelect' ? (
+          props.answerOptions?.map((option) => {
+            return (
+              <label className='form__label'>
+                {option}
+                <input
+                  type='radio'
+                  name='action'
+                  id=''
+                  required
+                  value={option}
+                  onChange={(e) => {
+                    setAnswer(e.target.value)
+                  }}
+                />
+              </label>
+            )
+          })
+        ) : (
+          <input
+            className='form__text-input'
+            type='text'
+            required
+            value={answer}
+            onChange={(e) => {
+              setAnswer(e.target.value)
+            }}
+          />
+        )}
+        <button className='form__button' type='submit'>
+          Submit Answer
+        </button>
+      </fieldset>
     </form>
   )
 }
